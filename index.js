29c1c4ea1a1c9ab1c67428073d2bbb1b00e7ad7a
Micah Wagner <micah.wagner@icloud.com>
2023-02-19 11:10:57 -0800

    added comments

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ let obj_h = 200;
 
 c.canvas.addEventListener("mousemove", function(e) {
 	if (mouseDown && keysDown["Shift"]) {
+		// add by a fraction of scale factor so rate of moving objects align with mouse
 		origin_x += e.movementX / scale_factor;
 		origin_y += e.movementY / scale_factor;
 	}
 });
 
 c.canvas.addEventListener("wheel", function(e) {
+	// set scale factor to some fraction of itself so zooming in feels constant
 	scale_factor += scale_factor * e.deltaY * sensitivity;
 	if (scale_factor < 0.0001) {
 		scale_factor = 0.0001;
@@ -46,12 +48,26 @@ render();
 
 function drawSquare(ctx, pos_x, pos_y, width, height, color) {
 	ctx.drawingContext.fillStyle = color;
+
+	// scale box width and height by scale factor
 	let widthScale = width * scale_factor;
 	let heightScale = height * scale_factor;
+
+	// this translates the position given relative to the world origin
+	// add half of screen dimentions so 0, 0  is in center of screen, then add position offset
 	let translation_X = (origin_x + screenWidth / 2) + pos_x;
 	let translation_Y = (origin_y + screenHeight / 2) + pos_y;
+
+	// this will scale the translated position depending on the zoom factor.
+	// the (translation_X - screenWidth/2) gets the vector from the center of the screen
+	// to the given center of the box, then we scale it by the scale factor (if we didn't do this, 
+	// the box would be at a constant position on the screen, more you zoom in the more you want objects to go away from the center, vice versa)
+	// then add this vector relative to the senter of the screen
 	let scale_X = screenWidth/2 + (translation_X - screenWidth/2) * scale_factor;
 	let scale_Y = screenHeight/2 + (translation_Y - screenHeight/2) * scale_factor;
+
+	// this is the final position of box
+	// subtract by the widScale/2 and heightScale so that fillRect properly draws rectanlge relative to center
 	let final_X = scale_X - widthScale / 2;
 	let final_Y = scale_Y - heightScale / 2;
 
